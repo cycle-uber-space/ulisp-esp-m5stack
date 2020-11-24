c8409a0f16b531f664648f30b4d7c4020f760661
Goheeca <goheeca@gmail.com>
2020-11-24 02:29:42 +0100

    Keep muffling errors if they were muffled beforehand

--- ulisp-esp.ino
+++ ulisp-esp.ino
@@ -2162,6 +2162,7 @@ object *sp_ignoreerrors (object *args, object *env) {
   handler = &dynamic_handler;
   object *result = nil;
 
+  bool muffled = tstflag(MUFFLEERRORS);
   setflag(MUFFLEERRORS);
   bool signaled = false;
   if (!setjmp(dynamic_handler)) {
@@ -2175,7 +2176,7 @@ object *sp_ignoreerrors (object *args, object *env) {
     signaled = true;
   }
   handler = previous_handler;
-  clrflag(MUFFLEERRORS);
+  if (!muffled) clrflag(MUFFLEERRORS);
 
   if (signaled) return symbol(NOTHING);
   else return result;
