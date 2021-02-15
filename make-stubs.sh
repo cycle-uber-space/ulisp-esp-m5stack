fb3492c6f6c238e3fe19eb2e681505f593dc449c
mgr <m.retzlaff@gmx.net>
2021-02-15 19:27:51 +0100

    add root_ca.h to make-stubs.sh and example template files
    in preparation for HTTP function

--- make-stubs.sh
+++ make-stubs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-test -e auth-token.h || touch auth-token.h
+test -e auth-token.h  || touch auth-token.h
+test -e root_ca.h     || touch root_ca.h
 test -e LispLibrary.h || ./wipe-lisp-library.sh
